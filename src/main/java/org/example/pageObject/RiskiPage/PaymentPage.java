d7d48fbb8404b71d5dc2b14a220936dfe025f7a7
riskiferdian <riskiferdian9@gmail.com>
2023-04-06 17:11:16 +0700

    add feature payement

---
+++ src/main/java/org/example/pageObject/RiskiPage/PaymentPage.java
@@ -0,0 +1,15 @@
+package org.example.pageObject.RiskiPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentPage {
+    public static WebDriver webDriver;
+    public PaymentPage(WebDriver webDriver){
+        PageFactory.initElements(webDriver, this);
+        this.webDriver = webDriver;
+    }
+
+    @FindBy()
+}
